e2aa190bd0f91107530765c83ff5118a74ee77a9
Mike Prince <mike@mikeprince.com>
2026-03-06 10:05:54 -0800

    Up to 0.7.3 with more helper functions

--- src/a2a-service/index.ts
+++ src/a2a-service/index.ts
@@ -2,3 +2,4 @@ export * from './executor-handler.js';
 export * from './router.js';
 export * from './types.js';
 export * from '../json-rpc-service/index.js';
+export * from './lite.js';
--- src/a2a-service/lite.ts
+++ src/a2a-service/lite.ts
@@ -1,5 +1,6 @@
 import { JsonRpcRequest, JsonRpcResponse } from "../json-rpc-client/types.js";
 import { jrpcError } from "../json-rpc-service/utils.js";
+import { AgentMessageEnvelope, parseDid } from "@agentic-profile/common";
 
 /**
  * Ensure the JSON-RPC request method is valid and return the method and params.
@@ -15,4 +16,25 @@ export function checkJrpcMethod( jrpcRequest: JsonRpcRequest, methods: string[] 
         return jrpcError(id, -32600, `Invalid method '${method}; only ${methods.join()} is supported`);
 
     return undefined;
+}
+
+export function resolveEnvelope(jrpcRequest: JsonRpcRequest): AgentMessageEnvelope {
+    const { params } = jrpcRequest;
+    if (!params)
+        throw new Error('Missing JSON-RPC param');
+        //return jrpcError(id, -32600, 'Missing JSON-RPC param');
+    const { message } = params;
+    const envelope = message?.metadata?.envelope ?? params?.metadata?.envelope;
+    if (!envelope)
+        throw new Error('Missing envelope in JSON-RPC param.metadata or param.message.metadata');
+        //return jrpcError(id, -32600, 'Missing envelope in JSON-RPC param.metadata or param.message.metadata');
+    
+    const { to: toAgentDid } = envelope;
+    if (!toAgentDid)
+        throw new Error("Message envelope is missing recipient agent did ('to' property)");
+    const { fragment } = parseDid(toAgentDid);
+    if (!fragment)
+        throw new Error("Invalid toAgentDid, missing fragment: " + toAgentDid);
+
+    return envelope
 }
